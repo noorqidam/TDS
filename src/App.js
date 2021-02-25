285ff7c83217de4165edf1f5277db1b7ac15203d
noorqidam <noorqidam@gmail.com>
2021-02-25 11:43:45 +0700

    add fetching api search users, style homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import User from './components/User'
 import './App.css';
+import Loading from './components/Loading'
+import Header from './components/Header'
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: '',
+      loading: false
+    }
+  }
+  getUser = () => {
+    this.setState({
+      loading: true
+    })
+    const name = this.refs.name.value;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    setTimeout( () => {
+      fetch(`http://api.github.com/users/${name}`)
+      .then(response => response.json())
+      .then(data => {
+
+        this.setState({
+          user: data,
+          loading: false
+        });
+      })
+    },1000)
+  }
+  render() {   
+    const name = this.state.user.name 
+    let userProfile;
+    if( this.state.loading === true)  {
+       userProfile = <div className='loading-user-card'><Loading /></div>
+    }else if (name ) {
+      userProfile = <User user={this.state.user} />
+    }
+    return (
+      <div className="App">
+        <Header />
+        <div className="wrapper">
+          <div id='search-bar'>
+            <input type="text" placeholder='Enter UserName' ref="name" />
+            <button className='searchButton' onClick={this.getUser}>
+              <i className="fas fa-search"></i>
+            </button>
+          </div>
+        </div>
+        {userProfile}
+      </div>
+    );
+  }
 }
 
 export default App;
